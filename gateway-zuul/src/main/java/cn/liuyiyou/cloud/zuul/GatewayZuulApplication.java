54104323df660d13505fd75fde2935b3ff71c9bc
liuyiyou <liuyiyou@ibalife.com>
2020-04-22 16:08:10 +0800

    Zuul网关

---
+++ gateway-zuul/src/main/java/cn/liuyiyou/cloud/zuul/GatewayZuulApplication.java
@@ -0,0 +1,23 @@
+package cn.liuyiyou.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableZuulProxy
+@SpringBootApplication
+@RestController
+public class GatewayZuulApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayZuulApplication.class, args);
+  }
+
+  @GetMapping("/")
+  public String home() {
+    return "gateway-zuul";
+  }
+
+}
---
+++ gateway-zuul/src/test/java/cn/liuyiyou/cloud/zuul/GatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.liuyiyou.cloud.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayZuulApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
