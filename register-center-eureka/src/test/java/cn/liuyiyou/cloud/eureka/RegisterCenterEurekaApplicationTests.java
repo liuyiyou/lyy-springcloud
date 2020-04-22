7124c9cc41fa11ff130fe03181d0dc5177fb5091
liuyiyou <liuyiyou@ibalife.com>
2020-04-22 15:56:45 +0800

    Eureka服务注册中心

---
+++ register-center-eureka/src/test/java/cn/liuyiyou/cloud/eureka/RegisterCenterEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.liuyiyou.cloud.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterCenterEurekaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
