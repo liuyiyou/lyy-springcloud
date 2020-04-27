99c6b1d12e0b4285881924f543b8828e01735cef
liuyiyou <liuyiyou@ibalife.com>
2020-04-27 14:26:32 +0800

    Feigh相关

---
+++ server-base-consumer/src/main/java/cn/liuyiyou/cloud/server/base/consumer/config/FeignConfiguration.java
@@ -0,0 +1,19 @@
+package cn.liuyiyou.cloud.server.base.consumer.config;
+
+import cn.liuyiyou.cloud.server.base.consumer.feign.FeignBasicAuthRequestInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: liuyiyou.cn
+ * @date: 2020/4/26
+ * @version: V1.0
+ */
+@Configuration
+public class FeignConfiguration {
+
+  @Bean
+  public FeignBasicAuthRequestInterceptor basicAuthRequestInterceptor(){
+    return  new FeignBasicAuthRequestInterceptor();
+  }
+}
---
+++ server-base-consumer/src/main/java/cn/liuyiyou/cloud/server/base/consumer/feign/FeignBasicAuthRequestInterceptor.java
@@ -0,0 +1,21 @@
+package cn.liuyiyou.cloud.server.base.consumer.feign;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Feign自定义拦截器
+ *
+ * @author: liuyiyou.cn
+ * @date: 2020/4/26
+ * @version: V1.0
+ */
+@Slf4j
+public class FeignBasicAuthRequestInterceptor implements RequestInterceptor {
+
+  @Override
+  public void apply(RequestTemplate requestTemplate) {
+    log.info("通过Feign调用会打印日志.......");
+  }
+}
